b2d5c3058a66b39261bd525aa4148c3d0a3c1ef0
DevExpressExampleBot <fakeEmail>
2022-12-17 03:24:51 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to filter a LookUpEdit by multiple columns
 ' 
 ' To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
@@ -7,7 +7,6 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5210
-
 ' Developer Express Code Central Example:
 ' How to filter a LookUpEdit by multiple columns
 ' 
@@ -17,55 +16,32 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5210
-
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports System.Globalization
 Imports DevExpress.Xpf.Grid
 
 Namespace Q412037
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			Dim gc As GridColumn
-			NamelookUpEdit.ItemsSource = New List(Of Item) From {
-				New Item With {
-					.ID = 012,
-					.ShortName = "q",
-					.LongName = "aaay"
-				},
-				New Item With {
-					.ID = 123,
-					.ShortName = "wn",
-					.LongName = "bbbh"
-				},
-				New Item With {
-					.ID = 234,
-					.ShortName = "e",
-					.LongName = "cccn"
-				}
-			}
-		End Sub
-	End Class
-	Public Class Item
-		Public Property ID() As Integer
-		Public Property ShortName() As String
-		Public Property LongName() As String
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim gc As GridColumn
+            Me.NamelookUpEdit.ItemsSource = New List(Of Item) From {New Item With {.ID = 012, .ShortName = "q", .LongName = "aaay"}, New Item With {.ID = 123, .ShortName = "wn", .LongName = "bbbh"}, New Item With {.ID = 234, .ShortName = "e", .LongName = "cccn"}}
+        End Sub
+    End Class
+
+    Public Class Item
+
+        Public Property ID As Integer
+
+        Public Property ShortName As String
+
+        Public Property LongName As String
+    End Class
 End Namespace
--- VB/MyLookUpEdit/MyLookUpEditStrategy.vb
+++ VB/MyLookUpEdit/MyLookUpEditStrategy.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to filter a LookUpEdit by multiple columns
 ' 
 ' To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
@@ -7,7 +7,6 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5210
-
 ' Developer Express Code Central Example:
 ' How to filter a LookUpEdit by multiple columns
 ' 
@@ -17,37 +16,35 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5210
-
 Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.Xpf.Grid.LookUp
 Imports DevExpress.Data.Filtering
-Imports DevExpress.Xpf.Grid
 
 Namespace Q412037
-	Public Class MyLookUpEditStrategy
-		Inherits LookUpEditStrategy
 
-		Public Sub New(ByVal editor As LookUpEdit)
-			MyBase.New(editor)
-		End Sub
+    Public Class MyLookUpEditStrategy
+        Inherits LookUpEditStrategy
 
-		Public Overrides Sub UpdateDisplayFilter()
-			Editor.Dispatcher.BeginInvoke(New Action(Sub() ItemsProvider.SetDisplayFilterCriteria(CreateDisplayFilterCriteria(AutoSearchText), Me.CurrentDataViewHandle)))
+        Public Sub New(ByVal editor As LookUpEdit)
+            MyBase.New(editor)
+        End Sub
 
-		End Sub
-		Protected Function CreateDisplayFilterCriteria(ByVal text As String) As CriteriaOperator
-			Dim lookUp As MyLookUpEdit = CType(Editor, MyLookUpEdit)
-			Dim groupOperator As New GroupOperator() With {.OperatorType = GroupOperatorType.Or}
-			For Each col As ColumnItem In lookUp.ColumnsToFilter
-				Dim list As New List(Of CriteriaOperator)()
-				list.Add(New OperandProperty() With {.PropertyName = col.FieldName})
-				list.Add(New OperandValue() With {.Value = text})
-				groupOperator.Operands.Add(New FunctionOperator(FunctionOperatorType.Contains, list))
-			Next col
-			Return groupOperator
-		End Function
-	End Class
+        Public Overrides Sub UpdateDisplayFilter()
+            Editor.Dispatcher.BeginInvoke(New Action(Sub() ItemsProvider.SetDisplayFilterCriteria(CreateDisplayFilterCriteria(AutoSearchText), CurrentDataViewHandle)))
+        End Sub
+
+        Protected Function CreateDisplayFilterCriteria(ByVal text As String) As CriteriaOperator
+            Dim lookUp As MyLookUpEdit = CType(Editor, MyLookUpEdit)
+            Dim groupOperator As GroupOperator = New GroupOperator() With {.OperatorType = GroupOperatorType.Or}
+            For Each col As ColumnItem In lookUp.ColumnsToFilter
+                Dim list As List(Of CriteriaOperator) = New List(Of CriteriaOperator)()
+                list.Add(New OperandProperty() With {.PropertyName = col.FieldName})
+                list.Add(New OperandValue() With {.Value = text})
+                groupOperator.Operands.Add(New FunctionOperator(FunctionOperatorType.Contains, list))
+            Next
+
+            Return groupOperator
+        End Function
+    End Class
 End Namespace
