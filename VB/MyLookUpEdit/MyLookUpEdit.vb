1669bc28be8c5df5bd575f1499de249fde95d561
DevExpressExampleBot <fakeEmail>
2023-02-28 18:03:08 +0400

    Source auto update [skip ci]

--- VB/MyLookUpEdit/MyLookUpEdit.vb
+++ VB/MyLookUpEdit/MyLookUpEdit.vb
@@ -1,36 +1,31 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.Xpf.Editors
 Imports DevExpress.Xpf.Grid.LookUp
 
 Namespace Q412037
-	Public Class MyLookUpEdit
-		Inherits LookUpEdit
-		Private columnsToFilter_Renamed As New List(Of ColumnItem)()
-		Public Property ColumnsToFilter() As List(Of ColumnItem)
-			Get
-				Return columnsToFilter_Renamed
-			End Get
-			Set(ByVal value As List(Of ColumnItem))
-				columnsToFilter_Renamed = value
-			End Set
-		End Property
-		Protected Overrides Function CreateEditStrategy() As EditStrategyBase
-			Return New MyLookUpEditStrategy(Me)
-		End Function
-	End Class
-	Public Class ColumnItem
-		Private privateFieldName As String
-		Public Property FieldName() As String
-			Get
-				Return privateFieldName
-			End Get
-			Set(ByVal value As String)
-				privateFieldName = value
-			End Set
-		End Property
-	End Class
+
+    Public Class MyLookUpEdit
+        Inherits LookUpEdit
+
+        Private columnsToFilterField As List(Of ColumnItem) = New List(Of ColumnItem)()
+
+        Public Property ColumnsToFilter As List(Of ColumnItem)
+            Get
+                Return columnsToFilterField
+            End Get
+
+            Set(ByVal value As List(Of ColumnItem))
+                columnsToFilterField = value
+            End Set
+        End Property
+
+        Protected Overrides Function CreateEditStrategy() As EditStrategyBase
+            Return New MyLookUpEditStrategy(Me)
+        End Function
+    End Class
+
+    Public Class ColumnItem
+
+        Public Property FieldName As String
+    End Class
 End Namespace
--- VB/MyLookUpEdit/MyLookUpEditStrategy.vb
+++ VB/MyLookUpEdit/MyLookUpEditStrategy.vb
@@ -1,28 +1,27 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.Xpf.Grid.LookUp
 Imports DevExpress.Data.Filtering
-Imports DevExpress.Xpf.Grid
 
 Namespace Q412037
-	Friend Class MyLookUpEditStrategy
-		Inherits LookUpEditStrategy
-		Public Sub New(ByVal editor As LookUpEdit)
-			MyBase.New(editor)
-		End Sub
-		Protected Overrides Function CreateDisplayFilterCriteria(ByVal text As String) As CriteriaOperator
-			Dim lookUp As MyLookUpEdit = CType(Editor, MyLookUpEdit)
-			Dim groupOperator As New GroupOperator() With {.OperatorType = GroupOperatorType.Or}
-			For Each col As ColumnItem In lookUp.ColumnsToFilter
-				Dim list As New List(Of CriteriaOperator)()
-				list.Add(New OperandProperty() With {.PropertyName = col.FieldName})
-				list.Add(New OperandValue() With {.Value = text})
-				groupOperator.Operands.Add(New FunctionOperator(FunctionOperatorType.Contains, list))
-			Next col
-			Return groupOperator
-		End Function
-	End Class
+
+    Friend Class MyLookUpEditStrategy
+        Inherits LookUpEditStrategy
+
+        Public Sub New(ByVal editor As LookUpEdit)
+            MyBase.New(editor)
+        End Sub
+
+        Protected Overrides Function CreateDisplayFilterCriteria(ByVal text As String) As CriteriaOperator
+            Dim lookUp As MyLookUpEdit = CType(Editor, MyLookUpEdit)
+            Dim groupOperator As GroupOperator = New GroupOperator() With {.OperatorType = GroupOperatorType.Or}
+            For Each col As ColumnItem In lookUp.ColumnsToFilter
+                Dim list As List(Of CriteriaOperator) = New List(Of CriteriaOperator)()
+                list.Add(New OperandProperty() With {.PropertyName = col.FieldName})
+                list.Add(New OperandValue() With {.Value = text})
+                groupOperator.Operands.Add(New FunctionOperator(FunctionOperatorType.Contains, list))
+            Next
+
+            Return groupOperator
+        End Function
+    End Class
 End Namespace
