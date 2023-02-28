0686d0fad9a61ea6de538801301bb946cd18c03c
AndreySlabov <andrey.slabov@devexpress.com>
2023-02-28 16:55:16 +0400

    Update Sample

--- CS/App.xaml.cs
+++ CS/App.xaml.cs
@@ -1,34 +1,6 @@
-﻿// Developer Express Code Central Example:
-// How to filter a LookUpEdit by multiple columns
-// 
-// To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-// descendant and override the UpdateDisplayFilter method where the filter criteria
-// is set.
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E5210
-
-// Developer Express Code Central Example:
-// How to filter a LookUpEdit by multiple columns
-// 
-// To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-// descendant and override the CreateDisplayFilterCriteria method where the filter
-// criteria is generated
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E5210
-
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Q412037 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application {
     }
 }
--- CS/MainWindow.xaml.cs
+++ CS/MainWindow.xaml.cs
@@ -1,52 +1,24 @@
-﻿// Developer Express Code Central Example:
-// How to filter a LookUpEdit by multiple columns
-// 
-// To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-// descendant and override the UpdateDisplayFilter method where the filter criteria
-// is set.
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E5210
-
-// Developer Express Code Central Example:
-// How to filter a LookUpEdit by multiple columns
-// 
-// To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-// descendant and override the CreateDisplayFilterCriteria method where the filter
-// criteria is generated
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E5210
-
-using System;
+﻿using DevExpress.Data.Filtering;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Globalization;
-using DevExpress.Xpf.Grid;
 
 namespace Q412037 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            GridColumn gc;
-            NamelookUpEdit.ItemsSource = new List<Item> {
+            lookUpEdit.ItemsSource = new List<Item> {
                 new Item { ID = 012, ShortName = "q", LongName = "aaay"},
                 new Item { ID = 123, ShortName = "wn", LongName = "bbbh"},
                 new Item { ID = 234, ShortName = "e", LongName = "cccn"},
             };
+        }
+        void lookUpEdit_SubstituteDisplayFilter(object sender, DevExpress.Xpf.Editors.Helpers.SubstituteDisplayFilterEventArgs e) {
+            if (string.IsNullOrEmpty(e.DisplayText))
+                return;
+            var shortNameFilter = CriteriaOperator.Parse("StartsWith(ShortName,?)", e.DisplayText);
+            var idFilter = CriteriaOperator.Parse("StartsWith(ID,?)", e.DisplayText);
+            e.DisplayFilter = new GroupOperator(GroupOperatorType.Or, idFilter, shortNameFilter, e.DisplayFilter);
+            e.Handled = true;
         }
     }
     public class Item {
