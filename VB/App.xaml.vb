a1442168af5d107c7d3ed04ef7bb1d692f7d981a
DevExpressExampleBot <fakeEmail>
2023-02-28 16:58:33 +0400

    Source auto update [skip ci]

--- VB/App.xaml.vb
+++ VB/App.xaml.vb
@@ -1,28 +1,7 @@
-' Developer Express Code Central Example:
-' How to filter a LookUpEdit by multiple columns
-' 
-' To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-' descendant and override the UpdateDisplayFilter method where the filter criteria
-' is set.
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E5210
-' Developer Express Code Central Example:
-' How to filter a LookUpEdit by multiple columns
-' 
-' To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-' descendant and override the CreateDisplayFilterCriteria method where the filter
-' criteria is generated
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E5210
 Imports System.Windows
 
 Namespace Q412037
 
-    ''' <summary>
-    ''' Interaction logic for App.xaml
-    ''' </summary>
     Public Partial Class App
         Inherits Application
 
--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,38 +1,23 @@
-' Developer Express Code Central Example:
-' How to filter a LookUpEdit by multiple columns
-' 
-' To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-' descendant and override the UpdateDisplayFilter method where the filter criteria
-' is set.
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E5210
-' Developer Express Code Central Example:
-' How to filter a LookUpEdit by multiple columns
-' 
-' To filter a LookUpEdit by multiple columns, create a LookUpEditStrategy
-' descendant and override the CreateDisplayFilterCriteria method where the filter
-' criteria is generated
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E5210
+Imports DevExpress.Data.Filtering
 Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Controls
-Imports DevExpress.Xpf.Grid
 
 Namespace Q412037
 
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
     Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
             Me.InitializeComponent()
-            Dim gc As GridColumn
-            Me.NamelookUpEdit.ItemsSource = New List(Of Item) From {New Item With {.ID = 012, .ShortName = "q", .LongName = "aaay"}, New Item With {.ID = 123, .ShortName = "wn", .LongName = "bbbh"}, New Item With {.ID = 234, .ShortName = "e", .LongName = "cccn"}}
+            Me.lookUpEdit.ItemsSource = New List(Of Item) From {New Item With {.ID = 012, .ShortName = "q", .LongName = "aaay"}, New Item With {.ID = 123, .ShortName = "wn", .LongName = "bbbh"}, New Item With {.ID = 234, .ShortName = "e", .LongName = "cccn"}}
+        End Sub
+
+        Private Sub lookUpEdit_SubstituteDisplayFilter(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.Helpers.SubstituteDisplayFilterEventArgs)
+            If String.IsNullOrEmpty(e.DisplayText) Then Return
+            Dim shortNameFilter = CriteriaOperator.Parse("StartsWith(ShortName,?)", e.DisplayText)
+            Dim idFilter = CriteriaOperator.Parse("StartsWith(ID,?)", e.DisplayText)
+            e.DisplayFilter = New GroupOperator(GroupOperatorType.Or, idFilter, shortNameFilter, e.DisplayFilter)
+            e.Handled = True
         End Sub
     End Class
 
